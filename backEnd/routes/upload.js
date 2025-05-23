1bf5456fe8763291ef7fa614fd56bf210ed1ce90
Aastha Gupta <itsaastha.2005@gmail.com>
2025-05-23 14:34:21 +0530

    changes

--- backEnd/routes/upload.js
+++ backEnd/routes/upload.js
@@ -7,3 +7,5 @@ const storage = multer.memoryStorage(); // or diskStorage if saving locally
 const uploadR= multer({ storage });
 
 router.post("/upload", uploadR.single("resume"), upload);
+
+module.exports = router;
