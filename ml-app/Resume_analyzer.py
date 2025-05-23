1ff13a7b949fb592763ed83a305f6d55b8d294c0
Aastha Gupta <itsaastha.2005@gmail.com>
2025-05-24 00:08:44 +0530

    fix: dynamic port issue

--- ml-app/Resume_analyzer.py
+++ ml-app/Resume_analyzer.py
@@ -203,4 +203,5 @@ async def analyze_resume(
 
 # -------------------------- Main Execution --------------------------
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
